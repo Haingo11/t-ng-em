f43429c91749776ee27eaaaa38a73991bb7e6c91
Haingo11 <93142075+Haingo11@users.noreply.github.com>
2021-11-28 00:15:52 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tôi  có điều này muốn hỏi Bạn, Bạn nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Tớ thích bạn lắm! bạn có thích tớ ko ? 😙',
+    desc: 'Bạn không trả lời hoặc thoát ra, tức là Bạn thích Tôi đó nha :> ',
+    btnYes: 'Có, tớ thật sự thích cậu 💖💖💖',
+    btnNo: '💔 mày bị điên  à ! 💔 ',
+    question:'lý do bạn thích tôi là gì vậy  😊😊',
+    reasonPlaceholder: 'trả lời',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì bạn đẹp trai đó Ahiiiii 😊😊',
+    mess: 'tôi biết mà, tôi cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100034813773285'
+}
